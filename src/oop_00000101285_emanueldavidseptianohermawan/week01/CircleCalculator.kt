21dba8d9849b48528e5321a9e5c641064029e04f
David <imanuel.david@student.umn.ac.id>
2026-02-09 22:17:39 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000101285_emanueldavidseptianohermawan/week01/CircleCalculator.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week01/CircleCalculator.kt
@@ -1,8 +1,8 @@
 package oop_00000101285_emanueldavidseptianohermawan.week01
 
-fun main(args: Array<String>) {
-    var radius: Double = 7.0
-    var pi: Double = 3.14
+fun main() {
+    var radius= 7.0
+    var pi= 3.14
 
     var area= pi * radius * radius
 
