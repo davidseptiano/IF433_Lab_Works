a4b344ac8871580706fbc9574768060094f71d54
David <imanuel.david@student.umn.ac.id>
2026-02-09 21:24:50 +0700

    week01: refactor to use val and string templates

--- src/oop_00000101285_emanueldavidseptianohermawan/week01/GradingSystem.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week01/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_00000101285_emanueldavidseptianohermawan.week01
 
 fun main() {
+    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
     var name: String = "John Thor"
     var score: Int = 80
 
     // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    println("Nama: $name, Nilai: $score")
 }
