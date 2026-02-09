cd6b2bfe374244c7bb9c197192e8c488c173c81e
David <imanuel.david@student.umn.ac.id>
2026-02-09 21:22:23 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000101285_emanueldavidseptianohermawan.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
