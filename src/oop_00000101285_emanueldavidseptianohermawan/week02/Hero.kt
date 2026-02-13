50252aa773bb14038d97e5222c593b6c506f1a5d
David <imanuel.david@student.umn.ac.id>
2026-02-13 15:22:49 +0700

    Commit 9: week02:  (task 2) create Hero class

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week02/Hero.kt
@@ -0,0 +1,18 @@
+package oop_00000101285_emanueldavidseptianohermawan.week02
+
+class Hero (
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+    fun attack(targatName: String) {
+        println("$name menebas $targatName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) hp = 0
+    }
+
+    fun isAlive(): Boolean = hp > 0
+}
