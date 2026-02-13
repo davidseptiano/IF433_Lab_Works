85d6020c6f0907f5653adbed7ce4731af4d4f9a4
David <imanuel.david@student.umn.ac.id>
2026-02-13 13:29:14 +0700

    Commit 1: week02: create basic Student class structure

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week02/Student.kt
@@ -0,0 +1,9 @@
+package oop_00000101285_emanueldavidseptianohermawan.week02
+
+class Student (
+    val name: String,
+    val nim: String,
+    val major: String,
+){
+    // Body class kosong dulu
+}
