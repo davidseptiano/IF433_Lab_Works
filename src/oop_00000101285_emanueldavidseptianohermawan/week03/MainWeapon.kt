a19ad95705b564c2a08cec91e83efc0219d3987c
David <imanuel.david@student.umn.ac.id>
2026-02-20 14:12:02 +0700

    Commit 10: week03: (task 2) implement Player testing in main

--- src/oop_00000101285_emanueldavidseptianohermawan/week03/MainWeapon.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week03/MainWeapon.kt
@@ -10,4 +10,15 @@ fun main() {
     println("Weapon: ${sword.name}")
     println("Damage: ${sword.damage}")
     println("Tier: ${sword.tier}")
+
+    println("\n=== TEST PLAYER ===")
+    val player = Player("Budi")
+
+    // player.xp -> harus error (jangan dihapus dari modul, cukup dikomentari)
+
+    player.addXp(50)
+    println("Level sekarang: ${player.level}")
+
+    player.addXp(60)
+    println("Level sekarang: ${player.level}")
 }
