e814b6419d84e9d706cbdcd778bdb115e7e08d5d
David <imanuel.david@student.umn.ac.id>
2026-02-20 13:18:07 +0700

    Commit 2: week03: simulate stack overflow error in setter

--- src/oop_00000101285_emanueldavidseptianohermawan/week03/Employee.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week03/Employee.kt
@@ -2,4 +2,10 @@ package oop_00000101285_emanueldavidseptianohermawan.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // PERINGATAN: KODE DIBAWAH INI SALAH & BERBAHAYA!
+            // Kita memanggil setter di dalam setter (Recursive)
+            this.salary = value
+        }
 }
---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000101285_emanueldavidseptianohermawan.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000 // Ini akan meledakan program And
+}
