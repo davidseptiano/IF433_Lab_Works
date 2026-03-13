0545c7e12f96dea25dfc03b60c68cd284705d0be
David <imanuel.david@student.umn.ac.id>
2026-03-13 15:54:20 +0700

    Commit 20: week06: (task) test smart home system logic in main

--- src/oop_00000101285_emanueldavidseptianohermawan/week06/Main.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week06/Main.kt
@@ -4,4 +4,13 @@ fun main() {
     val lamp = SmartLamp("1", "Ruang Tamu")
     val speaker = SmartSpeaker("2", "Google Nest Dapur")
     val cctv = SmartCCTV("3", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
 }
