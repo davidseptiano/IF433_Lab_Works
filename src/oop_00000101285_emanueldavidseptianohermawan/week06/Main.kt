7697d31831d5eb8250ffcf616e3942cacd1639c8
David <imanuel.david@student.umn.ac.id>
2026-03-13 15:53:40 +0700

    Commit 19: week06: (task) instantiate smart devices in main

--- src/oop_00000101285_emanueldavidseptianohermawan/week06/Main.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week06/Main.kt
@@ -1,21 +1,7 @@
 package oop_00000101285_emanueldavidseptianohermawan.week06
 
-fun processCheckout(method: PaymentMethod, amount: Double) {
-    println("-> Memulai checkout...")
-    method.pay(amount) // Dynamic polymorphism in action
-}
-
 fun main() {
-    val myWatch = Smartwatch()
-    myWatch.showTime()
-
-    val myPhone = Smartphone()
-    myPhone.turnOn()
-
-    val pay1 = Gopay()
-    val pay2 = CreditCard()
-
-    println("\n=== TESTING CHECKOUT ===")
-    processCheckout(pay1, 50000.0)
-    processCheckout(pay2, 150000.0)
+    val lamp = SmartLamp("1", "Ruang Tamu")
+    val speaker = SmartSpeaker("2", "Google Nest Dapur")
+    val cctv = SmartCCTV("3", "Ezviz Garasi")
 }
