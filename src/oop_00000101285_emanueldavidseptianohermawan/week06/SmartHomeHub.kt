f7a055714d22c88457548775a97ec89e3f426d03
David <imanuel.david@student.umn.ac.id>
2026-03-13 15:50:19 +0700

    Commit 17: week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week06/SmartHomeHub.kt
@@ -0,0 +1,31 @@
+package oop_00000101285_emanueldavidseptianohermawan.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
