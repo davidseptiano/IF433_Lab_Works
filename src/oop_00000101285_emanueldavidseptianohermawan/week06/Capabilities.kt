dd74534609d1d93998dfb693bdfcefd410e363b5
David <imanuel.david@student.umn.ac.id>
2026-03-13 15:22:31 +0700

    Commit 5: week06: create compound interfaces

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_00000101285_emanueldavidseptianohermawan.week06
+
+// Interface = Can-Do (Behavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable {
+    fun chargeBattery()
+}
