1dc8331f6c81333a28cdb5e2ad1cce339a2bfd8a
David <imanuel.david@student.umn.ac.id>
2026-03-27 13:43:07 +0700

    Commit 13: week07: (task) create ItemRarity enum

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000101285_emanueldavidseptianohermawan.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
