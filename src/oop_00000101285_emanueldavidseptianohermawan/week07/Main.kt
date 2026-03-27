bce795989335c6174df0dcb9f9767419b107746d
David <imanuel.david@student.umn.ac.id>
2026-03-27 13:25:22 +0700

    Commit 7: week07: implement copy and destructuring on data class

--- src/oop_00000101285_emanueldavidseptianohermawan/week07/Main.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week07/Main.kt
@@ -18,6 +18,11 @@ fun main() {
     println("\n=== TEST DATA CLASS ===")
     val data1 = DataUser("Alice", 22)
     val data2 = DataUser("Alice", 22)
+    val data3 = data1.copy(age = 23)
+    println("Hasil Copy: $data3")
+
+    val (userName, userAge) = data1
+    println("Destructured: $userName berumur $userAge")
     println(data1)
     println("Sama? ${data1 == data2}")
 }
