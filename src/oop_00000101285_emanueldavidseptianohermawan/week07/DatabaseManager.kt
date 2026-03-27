188730ad4ff13c7edc10c177b0db642fab2afe66
David <imanuel.david@student.umn.ac.id>
2026-03-27 13:03:47 +0700

    Commit 1: week07: create singleton DatabaseManager

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_00000101285_emanueldavidseptianohermawan.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}
