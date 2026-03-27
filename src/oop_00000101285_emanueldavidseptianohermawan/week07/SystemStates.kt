6c3cd319d6eae06422cb010cf1cf3ec9bc2491ea
David <imanuel.david@student.umn.ac.id>
2026-03-27 13:29:17 +0700

    Commit 9: week07: create ApiResponse sealed class

--- src/oop_00000101285_emanueldavidseptianohermawan/week07/SystemStates.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week07/SystemStates.kt
@@ -1,5 +1,14 @@
 package oop_00000101285_emanueldavidseptianohermawan.week07
 
+import javax.annotation.processing.Messager
+import javax.xml.crypto.Data
+
 enum class AppState {
     STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiResponse{
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
 }
