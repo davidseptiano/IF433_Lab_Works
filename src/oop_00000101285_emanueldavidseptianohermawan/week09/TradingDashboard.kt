13b414d5decde0f3cc423e96c66732e6fa048988
David <imanuel.david@student.umn.ac.id>
2026-04-24 14:12:14 +0700

    Commit 17: week09: (task) extract unique trading pairs using map and toSet

--- src/oop_00000101285_emanueldavidseptianohermawan/week09/TradingDashboard.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week09/TradingDashboard.kt
@@ -36,4 +36,9 @@ fun main() {
         .map {
             "LOSS [${it.pair} - ${it.position}]: ${it.roe}% ROE (Lev: ${it.leverage}x)"
         }
+
+    // ✅ Pipeline 6 (Unique Pair)
+    val uniquePairs = tradeHistory
+        .map { it.pair }
+        .toSet()
 }
