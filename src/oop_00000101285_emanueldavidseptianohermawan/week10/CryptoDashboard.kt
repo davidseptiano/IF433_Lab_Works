25fdae009c2ca29fb6b2c451fa83a730a6cf2e04
David <imanuel.david@student.umn.ac.id>
2026-05-08 14:04:33 +0700

    Commit 16: week10: (task) populate coin repository

--- src/oop_00000101285_emanueldavidseptianohermawan/week10/CryptoDashboard.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week10/CryptoDashboard.kt
@@ -4,4 +4,9 @@ fun main() {
 
     // Initialize coin repository
     val coinRepo = WalletRepository<Coin>()
+
+    // Populate coin repository
+    coinRepo.add(Coin("BTC", 0.25))
+    coinRepo.add(Coin("ETH", 2.5))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
