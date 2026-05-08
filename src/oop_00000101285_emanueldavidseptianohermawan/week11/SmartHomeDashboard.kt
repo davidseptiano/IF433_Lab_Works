1b4c1c29e085b3d9800aba901ba454fd9ae835bf
David <imanuel.david@student.umn.ac.id>
2026-05-08 15:12:25 +0700

    Commit 14: week11: (task) configure Philips WiZ using apply

--- src/oop_00000101285_emanueldavidseptianohermawan/week11/SmartHomeDashboard.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week11/SmartHomeDashboard.kt
@@ -4,4 +4,16 @@ fun main() {
 
     // Penampung perangkat
     val homeDevices = mutableListOf<SmartDevice>()
+
+    // =========================
+    // Konfigurasi Lampu Pintar
+    // =========================
+    SmartDevice("", "").apply {
+        name = "Philips WiZ Living Room"
+        category = "Lighting"
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
 }
