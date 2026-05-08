aea32f7d834bc3510a4e901e097718f6119444c7
David <imanuel.david@student.umn.ac.id>
2026-05-08 15:14:26 +0700

    Commit 16: week11: (task) configure AC unit using run

--- src/oop_00000101285_emanueldavidseptianohermawan/week11/SmartHomeDashboard.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week11/SmartHomeDashboard.kt
@@ -29,4 +29,30 @@ fun main() {
             println("(LOG) Kamera terhubung")
             homeDevices.add(it)
         }
+
+    // =========================
+    // Konfigurasi AC menggunakan run
+    // =========================
+    val acUnit = run {
+        SmartDevice(
+            "Daikin Inverter (Kabel 3x2.5)",
+            "HVAC",
+            false,
+            800
+        )
+    }
+
+    homeDevices.add(acUnit)
+
+    // =========================
+    // Tambah Auto Feeder
+    // =========================
+    homeDevices.add(
+        SmartDevice(
+            "Picolo's Auto Feeder",
+            "Pet Care",
+            true,
+            10
+        )
+    )
 }
