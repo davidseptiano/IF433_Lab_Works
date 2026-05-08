32b23deb172a0556aa6b509a4ab5f833a7d1327d
David <imanuel.david@student.umn.ac.id>
2026-05-08 15:15:18 +0700

    Commit 17: week11: (task) implement device search with let

--- src/oop_00000101285_emanueldavidseptianohermawan/week11/SmartHomeDashboard.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week11/SmartHomeDashboard.kt
@@ -55,4 +55,15 @@ fun main() {
             10
         )
     )
+
+    // =========================
+    // Pencarian Aman dengan let
+    // =========================
+    val searchResult = homeDevices.find {
+        it.category == "Camera"
+    }
+
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
