6e4ccc3dd7bdccb7d15964ff18d85dd8d9eaca05
David <imanuel.david@student.umn.ac.id>
2026-05-16 01:21:27 +0700

    Commit 5: week12: implement multiple catch blocks

--- src/oop_00000101285_emanueldavidseptianohermawan/week12/BasicException.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week12/BasicException.kt
@@ -23,4 +23,17 @@ fun main() {
     }
 
     println("Hasil parsing: $result")
+
+    println("\n=== TEST MULTIPLE CATCH ===")
+    val account = BankAccount(100.0)
+
+    try {
+        account.withdraw(150.0) // Ini akan memicu InsufficientFundsException
+    } catch (e: InsufficientFundsException) {
+        println("Caught Domain Error: Uang tidak cukup. ${e.message}")
+    } catch (e: IllegalArgumentException) {
+        println("Caught Argument Error: Input tidak valid. ${e.message}")
+    } catch (e: Exception) {
+        println("Caught General Error: Terjadi kesalahan tidak terduga.")
+    }
 }
