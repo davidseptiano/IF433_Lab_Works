7163f407f996399d40ed543bf251ff3cc3ac4c48
David <imanuel.david@student.umn.ac.id>
2026-05-16 01:33:16 +0700

    Commit 11: week12: (task) implement dispenseKibble function

---
+++ src/oop_00000101285_emanueldavidseptianohermawan/week12/SmartFeeder.kt
@@ -0,0 +1,12 @@
+package oop_00000101285_emanueldavidseptianohermawan.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+}
