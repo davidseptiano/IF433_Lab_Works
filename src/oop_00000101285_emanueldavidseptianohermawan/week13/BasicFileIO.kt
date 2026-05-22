126e2d4c02fe140ca1f677b61fd5d5effdcc0bdc
David <imanuel.david@student.umn.ac.id>
2026-05-22 13:29:07 +0700

    Commit 3: week13: implement readText and readLines (perbaikan)

--- src/oop_00000101285_emanueldavidseptianohermawan/week13/BasicFileIO.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week13/BasicFileIO.kt
@@ -4,7 +4,7 @@ import java.io.File
 fun main() {
     println("=== TEST WRITE TEXT ===")
     val file = File("notes.txt")
-    file.writerText("Line 1: Inisialisasi sistem.\n")
+    file.writeText("Line 1: Inisialisasi sistem.\n")
     println("File berhasil dibuat dan ditulis.")
     file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
     println("Teks berhasil di-append.")
