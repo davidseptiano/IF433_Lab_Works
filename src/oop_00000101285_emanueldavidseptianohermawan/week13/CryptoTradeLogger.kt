83719c97d6705891c06da406a117ff2d16ad3645
David <imanuel.david@student.umn.ac.id>
2026-05-22 14:50:35 +0700

    Commit 16: week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_00000101285_emanueldavidseptianohermawan/week13/CryptoTradeLogger.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week13/CryptoTradeLogger.kt
@@ -42,3 +42,15 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         }
     }
 }
+
+// 6. Load System (Safe Skip)
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path)
+            .readLines()
+            .mapNotNull { fromCsvTrade(it) }
+    } catch (e: FileNotFoundException) {
+        println("(Log) File tidak ditemukan.")
+        emptyList()
+    }
+}
