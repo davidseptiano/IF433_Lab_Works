854011e2667ea94548673b2dda107873302ac45e
David <imanuel.david@student.umn.ac.id>
2026-05-22 14:48:50 +0700

    Commit 13 & 14: week13: (task) implement fromCsvTrade with split & (task) wrap fromCsvTrade in try-catch for NumberFormatException

--- src/oop_00000101285_emanueldavidseptianohermawan/week13/CryptoTradeLogger.kt
+++ src/oop_00000101285_emanueldavidseptianohermawan/week13/CryptoTradeLogger.kt
@@ -15,3 +15,21 @@ data class TradeRecord(
 fun TradeRecord.toCsv(): String {
     return "$id,$symbol,$type,$margin,$pnl"
 }
+
+// 3 & 4. Deserialization + Error Handling
+fun fromCsvTrade(line: String): TradeRecord? {
+    return try {
+        val parts = line.split(",")
+
+        TradeRecord(
+            id = parts[0].toInt(),
+            symbol = parts[1],
+            type = parts[2],
+            margin = parts[3].toDouble(),
+            pnl = parts[4].toDouble()
+        )
+    } catch (e: Exception) {
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
+}
